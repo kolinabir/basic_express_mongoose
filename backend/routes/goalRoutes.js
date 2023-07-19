22b2b823da0eac6a0efeeea1a7afef41795d61d3
kolinabir <knkolin9@gmail.com>
2023-07-20 02:17:49 +0600

    together in one line added -check goalRoutes.js

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { getGoals,setGoal,updateGoal,deleteGoal } = require("../controllers/goalController");
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 
-router.get("/", getGoals);
-router.post("/", setGoal);
-router.put("/:id", updateGoal);
-router.delete("/:id", deleteGoal);
+// router.get("/", getGoals);
+// router.post("/", setGoal);
+// router.put("/:id", updateGoal);
+// router.delete("/:id", deleteGoal);
+// together in one line
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
